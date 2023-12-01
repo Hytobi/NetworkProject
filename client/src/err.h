e5de60ac12b56c293cab48bf294140ef73d717b2
Hana Delcourt <hanadelcourt@gmail.com>
2023-12-01 15:14:01 +0100

    client err.h

---
+++ client/src/err.h
@@ -0,0 +1,12 @@
+//
+// Created by hana on 01/12/23.
+//
+
+#ifndef PROJECT_ERR_H
+#define PROJECT_ERR_H
+
+#define ERR -1
+
+#define EXIT_IF_FAIL(x,msg) if (x == -1) { perror(msg); exit(EXIT_FAILURE); }
+
+#endif //PROJECT_ERR_H
